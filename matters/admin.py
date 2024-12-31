cf893f4568a1aa5a87133dbf5fdce1e1ba91979c
SoroushGReza <soroush_developer@hotmail.com>
2024-12-31 17:53:58 +0300

    Enhanced admin interface to manage Matter and Matt erFile:
    - Added `title` to the `list_display` for better visibility of `Matter`
      titles in the admin panel.
    - No changes to filters or search fields, maintaining functionality for
      filtering by `status`, `created_at`, and `user`.
    - Created a new `MatterFileAdmin` class to manage `MatterFile` entries
      in the admin panel.
    - Added `list_display` fields: `id`, `matter`, `file`, and `uploaded_at`
      to provide comprehensive information on uploaded files.
    - Enabled search functionality for `file` and `matter__title` to
      simplify finding specific files.

--- matters/admin.py
+++ matters/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Matter
+from .models import Matter, MatterFile
 
 
 @admin.register(Matter)
 class MatterAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "status", "created_at"]
+    list_display = ["id", "user", "title", "status", "created_at"]
     list_filter = ["status", "created_at", "user"]
     search_fields = ["user__email", "description"]
+
+
+@admin.register(MatterFile)
+class MatterFileAdmin(admin.ModelAdmin):
+    list_display = ["id", "matter", "file", "uploaded_at"]
+    search_fields = ["file", "matter__title"]
