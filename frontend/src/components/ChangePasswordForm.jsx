4d32108f9098bc8c0c6487f020447010688a67d4
SoroushGReza <soroush_developer@hotmail.com>
2025-01-15 15:11:42 +0300

    FIX: Remove mb-4 class to hide box-shadow overflow

--- frontend/src/components/ChangePasswordForm.jsx
+++ frontend/src/components/ChangePasswordForm.jsx
@@ -4,40 +4,46 @@ import styles from "../styles/Profile.module.css";
 
 function ChangePasswordForm({ passwordData, onChange, onSubmit }) {
   return (
-    <Form className={`${styles.profileForm} mb-4`} onSubmit={onSubmit}>
-      <h1 className={`${styles["formHeader"]} text-center`}>Ändra Lösenord</h1>
-      <Form.Group controlId="oldPassword" className="mb-3">
-        <Form.Label className={`${styles.formLabel}`}>
-          Gammalt lösenord
-        </Form.Label>
-        <Form.Control
-          className={`${styles.formInput}`}
-          type="password"
-          name="old_password"
-          value={passwordData.old_password}
-          onChange={onChange}
-          required
-        />
-      </Form.Group>
-      <Form.Group controlId="newPassword" className="mb-3">
-        <Form.Label className={`${styles.formLabel}`}>Nytt lösenord</Form.Label>
-        <Form.Control
-          className={`${styles.formInput}`}
-          type="password"
-          name="new_password"
-          value={passwordData.new_password}
-          onChange={onChange}
-          required
-        />
-      </Form.Group>
-      <Button
-        className={`${styles.updatePasswordButton} mb-3`}
-        variant="warning"
-        type="submit"
-      >
-        Byt lösenord
-      </Button>
-    </Form>
+    <div className={styles.formWrapper}>
+      <Form className={`${styles.profileForm}`} onSubmit={onSubmit}>
+        <h1 className={`${styles["formHeader"]} text-center`}>
+          Ändra Lösenord
+        </h1>
+        <Form.Group controlId="oldPassword" className="mb-3">
+          <Form.Label className={`${styles.formLabel}`}>
+            Gammalt lösenord
+          </Form.Label>
+          <Form.Control
+            className={`${styles.formInput}`}
+            type="password"
+            name="old_password"
+            value={passwordData.old_password}
+            onChange={onChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="newPassword" className="mb-3">
+          <Form.Label className={`${styles.formLabel}`}>
+            Nytt lösenord
+          </Form.Label>
+          <Form.Control
+            className={`${styles.formInput}`}
+            type="password"
+            name="new_password"
+            value={passwordData.new_password}
+            onChange={onChange}
+            required
+          />
+        </Form.Group>
+        <Button
+          className={`${styles.updatePasswordButton} mb-3`}
+          variant="warning"
+          type="submit"
+        >
+          Byt lösenord
+        </Button>
+      </Form>
+    </div>
   );
 }
 
--- frontend/src/components/ProfileForm.jsx
+++ frontend/src/components/ProfileForm.jsx
@@ -4,57 +4,63 @@ import styles from "../styles/Profile.module.css";
 
 function ProfileForm({ profileData, onChange, onSubmit }) {
   return (
-    <Form className={`${styles.profileForm} mb-4`} onSubmit={onSubmit}>
-      <h1 className={`${styles["formHeader"]} text-center`}>Uppdatera Profil</h1>
-      <Form.Group controlId="profileEmail" className="mb-3">
-        <Form.Label className={`${styles.formLabel}`}>E-post</Form.Label>
-        <Form.Control
-          className={`${styles.formInput}`}
-          type="email"
-          name="email"
-          value={profileData.email}
-          onChange={onChange}
-          required
-        />
-      </Form.Group>
-      <Form.Group controlId="profileName" className="mb-3">
-        <Form.Label className={`${styles.formLabel}`}>Förnamn</Form.Label>
-        <Form.Control
-          className={`${styles.formInput}`}
-          type="text"
-          name="name"
-          value={profileData.name}
-          onChange={onChange}
-        />
-      </Form.Group>
-      <Form.Group controlId="profileSurname" className="mb-3">
-        <Form.Label className={`${styles.formLabel}`}>Efternamn</Form.Label>
-        <Form.Control
-          className={`${styles.formInput}`}
-          type="text"
-          name="surname"
-          value={profileData.surname}
-          onChange={onChange}
-        />
-      </Form.Group>
-      <Form.Group controlId="profilePhone" className="mb-3">
-        <Form.Label className={`${styles.formLabel}`}>Telefonnummer</Form.Label>
-        <Form.Control
-          className={`${styles.formInput}`}
-          type="text"
-          name="phone_number"
-          value={profileData.phone_number}
-          onChange={onChange}
-        />
-      </Form.Group>
-      <Button
-        variant="primary"
-        type="submit"
-        className={`${styles.updateProfileButton} mb-3`}
-      >
-        Uppdatera profil
-      </Button>
-    </Form>
+    <div className={styles.formWrapper}>
+      <Form className={`${styles.profileForm}`} onSubmit={onSubmit}>
+        <h1 className={`${styles["formHeader"]} text-center`}>
+          Uppdatera Profil
+        </h1>
+        <Form.Group controlId="profileEmail" className="mb-3">
+          <Form.Label className={`${styles.formLabel}`}>E-post</Form.Label>
+          <Form.Control
+            className={`${styles.formInput}`}
+            type="email"
+            name="email"
+            value={profileData.email}
+            onChange={onChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="profileName" className="mb-3">
+          <Form.Label className={`${styles.formLabel}`}>Förnamn</Form.Label>
+          <Form.Control
+            className={`${styles.formInput}`}
+            type="text"
+            name="name"
+            value={profileData.name}
+            onChange={onChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="profileSurname" className="mb-3">
+          <Form.Label className={`${styles.formLabel}`}>Efternamn</Form.Label>
+          <Form.Control
+            className={`${styles.formInput}`}
+            type="text"
+            name="surname"
+            value={profileData.surname}
+            onChange={onChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="profilePhone" className="mb-3">
+          <Form.Label className={`${styles.formLabel}`}>
+            Telefonnummer
+          </Form.Label>
+          <Form.Control
+            className={`${styles.formInput}`}
+            type="text"
+            name="phone_number"
+            value={profileData.phone_number}
+            onChange={onChange}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="submit"
+          className={`${styles.updateProfileButton} mb-3`}
+        >
+          Uppdatera profil
+        </Button>
+      </Form>
+    </div>
   );
 }
 
