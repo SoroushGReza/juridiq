b7b149f769ae31c0e2bcfdd3eda6b2a4b473e599
SoroushGReza <soroush_developer@hotmail.com>
2025-02-11 15:20:17 +0300

    Correct comment from "Blackblaze" to "Cloudflare"

--- backend/settings.py
+++ backend/settings.py
@@ -225,7 +225,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# Blackblaze
+# Cloudflare
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
