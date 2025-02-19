24c6f55613f8815411598194638ca41ab7f7e490
SoroushGReza <soroush_developer@hotmail.com>
2025-02-19 15:42:55 +0300

    Updated development link during development

--- accounts/adapter.py
+++ accounts/adapter.py
@@ -5,7 +5,7 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     # PRODUCTION
     def get_email_confirmation_url(self, request, emailconfirmation):
         return f"https://juridiq.nu/verify-email/{emailconfirmation.key}"
-    
+
     # DEVELOPMENT
     # def get_email_confirmation_url(self, request, emailconfirmation):
-    #     return f"http://192.168.1.5:5173/verify-email/{emailconfirmation.key}"
+    #     return f"http://192.168.1.24:5173/verify-email/{emailconfirmation.key}"
